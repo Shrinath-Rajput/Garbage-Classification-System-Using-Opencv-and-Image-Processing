e73abd59fde3bd04e6a977ca0f000459f8b777d3
Shrinath Rajput <rajputshrinath349@gmail.com>
2026-04-25 12:21:36 +0530

    final fix railway start

--- backend/index.js
+++ backend/index.js
@@ -111,6 +111,6 @@ app.get("/clear", (req, res) => {
 // ================== SERVER ==================
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
     console.log("🚀 Server running on port", PORT);
 });
